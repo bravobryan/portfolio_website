2c0e0a0f7690a61995fbfd438fac5257786a8810
bravobryan <120871658+bravobryan@users.noreply.github.com>
2024-05-23 09:36:09 -0700

    Making changes

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 #- $env:FLASK_ENV="developer"  to turn on debug mode on.
 
 @app.route("/")
-def hello_world():
-    return "<p>This is Bryan's Portfolio!</p>"
+def index():
+    return render_template('index.html')
