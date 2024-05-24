2682ace8eda2027a450db4e2ead8ddbc5dee41e9
bravobryan <120871658+bravobryan@users.noreply.github.com>
2024-05-24 09:40:36 -0700

    Releasing first version of website.

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
-##-- IN POWERSHELL --##
-#- $env:FLASK_APP = "main"
-#- flask run
-
-#- $env:FLASK_ENV="developer"  to turn on debug mode on.
-
 @app.route("/")
 def index():
     return render_template('index.html')
@@ -16,12 +10,12 @@ def index():
 def html_page(page_name):
     return render_template(page_name)
 
-def write_to_file(data):
-    with open('database.csv', 'a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f"\n{email},{subject},{message}")
+# def write_to_file(data):
+#     with open('database.csv', 'a') as database:
+#         email = data['email']
+#         subject = data['subject']
+#         message = data['message']
+#         file = database.write(f"\n{email},{subject},{message}")
 
 # @app.route('/submit_form', methods=['POST', 'GET'])
 # def submit_form():
@@ -31,6 +25,11 @@ def write_to_file(data):
 #         return redirect('/thankyou.html')
 #     else:
 #         return "Something went wrong, try again!"
+
+
+##-- IN POWERSHELL --##
+#- $env:FLASK_APP = "main"
+#- flask run --debug
 
 ##-- Code for BASH --##
 # git init
